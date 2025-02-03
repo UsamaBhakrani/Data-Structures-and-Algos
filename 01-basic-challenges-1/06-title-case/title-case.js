ddee12e5d17b099e576fe3ace57d3278e8e1d30f
Usama Bhakrani <usamabhakrani@gmail.com>
2025-02-03 21:31:25 +0500

    Return uppercase letters

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,9 @@
-function titleCase() {}
+function titleCase(str) {
+  const words = str.toLowerCase().split(" ");
+  const capitalizedWords = words.map(
+    (word) => word.charAt(0).toUpperCase() + word.slice(1)
+  );
+  return capitalizedWords.join(" ");
+}
 
 module.exports = titleCase;
