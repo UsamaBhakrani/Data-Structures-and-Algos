2f072120690830cdf35b2ca30d2bd5caa75a49ba
Usama Bhakrani <usamabhakrani@gmail.com>
2025-02-03 21:15:43 +0500

    calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,20 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  let result;
+  if (operator === "+") {
+    result = num1 + num2;
+  } else if (operator === "-") {
+    result = num1 - num2;
+  } else if (operator === "*") {
+    result = num1 * num2;
+  } else if (operator === "/") {
+    if (num2 === 0) {
+      throw new Error("Division by zero is not allowed.");
+    }
+    result = num1 / num2;
+  } else {
+    throw new Error("Invalid operator. Please use +, -, *, or /");
+  }
+  return result;
+}
 
 module.exports = calculator;
